d2e16411851b6bcd30fc98a532ecebb8e91e3bb0
sakila161105 <nandasakila1605@gmail.com>
2025-09-20 18:07:36 +0700

    update page tsx dan blogcard

--- src/app/blog/[id]/[title]/page.tsx
+++ src/app/blog/[id]/[title]/page.tsx
@@ -1,11 +1,22 @@
-interface BlogProp {
-    params: { id: string; title: string };
+import BlogCard from "../components/BlogCard";
+
+interface PageProps {
+    params: {
+        id: string;
+        title: string;
+    };
 }
 
-export default function BlogDetail({ params }: BlogProp) {
+export default function Page({ params }: PageProps) {
     return (
-        <h1>
-            Detail Blog dengan ID: {params.id} - Judul: {params.title}
-        </h1>
+        <div className="p-6 space-y-4">
+            <h1 className="text-3xl font-bold mb-6">Daftar Blog</h1>
+
+            <BlogCard
+                id={params.id}
+                title={params.title}
+                description="Artikel ini berisi pembahasan penting terkait sejarah dan inspirasi."
+            />
+        </div>
     );
 }
--- src/app/blog/[id]/components/BlogCard.tsx
+++ src/app/blog/[id]/components/BlogCard.tsx
@@ -1,24 +1,17 @@
 "use client";
 
-export default function BlogCardComponent({
+export default function BlogCard({
     id,
     title,
     description,
-    href,
 }: {
     id: string;
     title: string;
     description: string;
-    href?: string;
 }) {
     return (
-        <>
-        <div
-            id={id}
-            className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm 
-                 dark:bg-gray-800 dark:border-gray-700"
-        >
-            <a href={href || "#"}>
+        <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
+            <a href={`/blog/${id}/${title}`}>
                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                     {title}
                 </h5>
@@ -27,10 +20,8 @@ export default function BlogCardComponent({
                 {description}
             </p>
             <a
-                href={href || "#"}
-                className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white 
-                   bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none 
-                   focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                href={`/blog/${id}/${title}`}
+                className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
                 Read more
                 <svg
@@ -50,6 +41,5 @@ export default function BlogCardComponent({
                 </svg>
             </a>
         </div>
-        </>
     );
 }
